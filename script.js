bfda1eef27c15a37ea03e270beb025133ffe7a4d
divyang1507 <divhp1507@gmail.com>
2024-07-22 17:40:57 +0530

    Stagger in hero headlink

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const tl = gsap.timeline()
+
 gsap.to("nav", {
   backgroundColor: "#000",
   color: '#ffffff', 
@@ -21,5 +23,25 @@ gsap.to("a", {
     start : 'top -10%',
     end : 'top -10',
     scrub : 1
+  }
+});
+gsap.from(".h1container h1", {
+  duration : 0.5,
+  y: 400,
+  ease: "power1.out",
+  stagger : 0.2
+
+})
+
+tl.to(".h1container h1", {
+ duration : 0.5,
+  y: 400,
+  scrollTrigger : {
+    trigger  : '.h1container h1',
+    scroller : 'body',
+    // markers : true,
+    start : 'top -10%',
+    end : 'top -10',
+    scrub : 2
   }
 });
